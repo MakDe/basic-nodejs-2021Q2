0db0250baecce6367777909f46cf6c7da136f82a
makde <d.maksimov@websecret.by>
2021-05-16 23:48:00 +0300

    fix resp error

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -11,7 +11,7 @@ router.route('/').get(async (req, res) => {
 router.route('/:id').get(async (req, res) => {
   const board = await boardService.getBoard(req.params.id);
 
-  res.status(200).send(board);
+  res.status(board ? 200 : 404).send(board);
 });
 
 router.route('/').post(async (req, res) => {
@@ -27,8 +27,7 @@ router.route('/:id').delete(async (req, res) => {
 });
 
 router.route('/:id').put(async (req, res) => {
-  const board = await boardService.updateBoard(Board.fromRequest(req.body)
-  );
+  const board = await boardService.updateBoard(Board.fromRequest(req.body));
 
   res.status(200).send(board);
 });
--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -11,7 +11,7 @@ router.route('/').get(async (req, res) => {
 router.route('/:id').get(async (req, res) => {
   const task = await taskService.getTask(req.params.boardId, req.params.id);
 
-  res.status(200).send(task);
+  res.status(task ? 200 : 404).send(task);
 });
 
 router.route('/').post(async (req, res) => {
@@ -25,7 +25,7 @@ router.route('/').post(async (req, res) => {
 router.route('/:id').delete(async (req, res) => {
   await taskService.removeTask(req.params.boardId, req.params.id);
 
-  res.sendStatus(204)
+  res.sendStatus(204);
 });
 
 router.route('/:id').put(async (req, res) => {
