48b4fb348bf82f6a55f476a73d374b40a9fd27ee
Denis Maksimov <d.maksimov@websecret.by>
2021-06-06 18:16:00 +0300

    request logger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import YAML from 'yamljs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import loggerMiddleware from './middlewares/requestLogger';
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -20,6 +21,8 @@ app.use('/', (req, res, next) => {
   }
   next();
 });
+
+app.use(loggerMiddleware);
 
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
